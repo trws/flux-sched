997e492b3a3bfe3ab0ff6887b1915bef31cfe31c
Daniel Milroy <daniel.milroy@gmail.com>
2024-04-04 19:00:06 -0700

    traverser: delete job2span entry for removed jobid
    Problem: the current implementation of rem_agfilter doesn't remove the
    job2span entry when removing a job. Surprisingly, this hasn't caused
    test failures or production problems.
    
    Add the approrpiate removal of the job2span entry.

--- resource/traversers/dfu_impl_update.cpp
+++ resource/traversers/dfu_impl_update.cpp
@@ -420,6 +420,7 @@ int dfu_impl_t::rem_agfilter (vtx_t u, int64_t jobid, const std::string &subsyst
         m_err_msg += strerror (errno);
         m_err_msg += "\n";
     }
+    job2span.erase (jobid);
 
 done:
     return rc;
